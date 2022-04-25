47e92bca0e00e107128244a393890c146e824f1e
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-04-25 17:48:06 +0500

    first commit

---
+++ programs/basic-anchor/src/lib.rs
@@ -0,0 +1,64 @@
+use anchor_lang::prelude::*;
+
+declare_id!("yCucSuJT8KeJidTFFHQTYQ4JBTMHq4Q6zNv8RZMjEKV");
+
+#[program]
+mod basic_anchor {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = 0;
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<Update>, data: u64) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data = data;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        let my_account = &mut ctx.accounts.my_account;
+        my_account.data -= 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub my_account: Account<'info, MyAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut)]
+    pub my_account: Account<'info, MyAccount>,
+}
+
+#[account]
+pub struct MyAccount {
+    pub data: u64,
+}
---
+++ tests/basic-anchor.js
@@ -0,0 +1,84 @@
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
+
+describe("basic-anchor", () => {
+  // Use a local provider.
+  const provider = anchor.AnchorProvider.local();
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(provider);
+
+  let _myAccount = null;
+
+  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
+    // #region code-simplified
+    // The program to execute.
+    const idl = JSON.parse(
+      require("fs").readFileSync("./target/idl/basic_anchor.json", "utf8")
+    );
+
+    //Address of the deployed program
+    const programId = new anchor.web3.PublicKey("yCucSuJT8KeJidTFFHQTYQ4JBTMHq4Q6zNv8RZMjEKV");
+
+    //Generate the program client from IDL
+    const program = new anchor.Program(idl, programId);
+
+    // The Account to create.
+    const myAccount = anchor.web3.Keypair.generate();
+
+    // Create the new account and initialize it with the program.
+    // #region code-simplified
+    await program.rpc.initialize({
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [myAccount],
+    });
+    // #endregion code-simplified
+
+    // Fetch the newly created account from the cluster.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check it's state was initialized.
+    assert.equal(account.data, 0);
+
+    // Store the account for the next test.
+    _myAccount = myAccount;
+  });
+
+  it("Updates a previously created account", async () => {
+    const myAccount = _myAccount;
+
+    // #region update-test
+
+    // The program to execute.
+    const idl = JSON.parse(
+      require("fs").readFileSync("./target/idl/basic_anchor.json", "utf8")
+    );
+
+    //Address of the deployed program
+    const programId = new anchor.web3.PublicKey("yCucSuJT8KeJidTFFHQTYQ4JBTMHq4Q6zNv8RZMjEKV");
+
+    //Generate the program client from IDL
+    const program = new anchor.Program(idl, programId);
+
+    // Invoke the update rpc.
+    await program.rpc.update(new anchor.BN(100), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+      },
+    });
+
+    // Fetch the newly updated account.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check it's state was mutated.
+    assert.equal(account.data, 100);
+
+    // #endregion update-test
+  });
+});
+
