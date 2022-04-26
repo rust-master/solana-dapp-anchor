ff76bc53d31b995c8e7b9bf5da0ffdfaa4b558fd
Zaryab-Programmer <muhammadzaryabrafique@gmail.com>
2022-04-26 13:00:40 +0500

    transctions code added

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,29 @@
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+    ConnectionProvider,
+    useAnchorWallet,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+    WalletModalProvider,
+    WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 import {
     GlowWalletAdapter,
     PhantomWalletAdapter,
     SlopeWalletAdapter,
     SolflareWalletAdapter,
     TorusWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import React, { FC, ReactNode, useMemo } from 'react';
+} from "@solana/wallet-adapter-wallets";
+import { Program, Provider, web3, BN } from "@project-serum/anchor";
+import { clusterApiUrl, Connection } from "@solana/web3.js";
+import React, { FC, ReactNode, useMemo } from "react";
+import idl from "./idl.json";
 
-require('./App.css');
-require('@solana/wallet-adapter-react-ui/styles.css');
+console.log(idl);
+
+require("./App.css");
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 const App: FC = () => {
     return (
@@ -54,8 +65,132 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 };
 
 const Content: FC = () => {
+    const wallet = useAnchorWallet();
+    const baseAccount = web3.Keypair.generate();
+
+    function getProvider() {
+        if (!wallet) {
+            return null;
+        }
+
+        const network = "http://127.0.0.1:8899";
+        const connection = new Connection(network, "processed");
+
+        const provider = new Provider(connection, wallet, {
+            preflightCommitment: "processed",
+        });
+
+        return provider;
+    }
+
+    async function createCounter() {
+        const provider = getProvider();
+
+        if (!provider) {
+            return;
+        }
+
+        const a = JSON.stringify(idl);
+        const b = JSON.parse(a);
+        const program = new Program(b, idl.metadata.address, provider);
+        try {
+            await program.rpc.initialize({
+                accounts: {
+                    myAccount: baseAccount.publicKey,
+                    user: provider.wallet.publicKey,
+                    systemProgram: web3.SystemProgram.programId,
+                },
+                signers: [baseAccount]
+            });
+
+            const account = await program.account.myAccount.fetch(baseAccount.publicKey);
+            console.log('account: ', account);
+        }
+        catch (err) {
+            console.log("Transcation error: ", err);
+        }
+    }
+
+    async function increment() {
+        const provider = getProvider();
+
+        if (!provider) {
+            return;
+        }
+
+        const a = JSON.stringify(idl);
+        const b = JSON.parse(a);
+        const program = new Program(b, idl.metadata.address, provider);
+        try {
+            await program.rpc.increment({
+                accounts: {
+                    myAccount: baseAccount.publicKey,
+                },
+            });
+
+            const account = await program.account.myAccount.fetch(baseAccount.publicKey);
+            console.log('account: ', account.data.toString());
+        }
+        catch (err) {
+            console.log("Transcation error: ", err);
+        }
+    }
+
+    async function decrement() {
+        const provider = getProvider();
+
+        if (!provider) {
+            return;
+        }
+
+        const a = JSON.stringify(idl);
+        const b = JSON.parse(a);
+        const program = new Program(b, idl.metadata.address, provider);
+        try {
+            await program.rpc.decrement({
+                accounts: {
+                    myAccount: baseAccount.publicKey,
+                },
+            });
+
+            const account = await program.account.myAccount.fetch(baseAccount.publicKey);
+            console.log('account: ', account.data.toString());
+        }
+        catch (err) {
+            console.log("Transcation error: ", err);
+        }
+    }
+
+    async function update() {
+        const provider = getProvider();
+        if (!provider) {
+            return;
+        }
+
+        const a = JSON.stringify(idl);
+        const b = JSON.parse(a);
+        const program = new Program(b, idl.metadata.address, provider);
+        try {
+            await program.rpc.update(new BN(100), {
+                accounts: {
+                    myAccount: baseAccount.publicKey,
+                },
+            });
+
+            const account = await program.account.myAccount.fetch(baseAccount.publicKey);
+            console.log('account: ', account.data.toString());
+        }
+        catch (err) {
+            console.log("Transcation error: ", err);
+        }
+    }
+
     return (
         <div className="App">
+            <button onClick={createCounter}>Initialize</button>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={update}>Update</button>
             <WalletMultiButton />
         </div>
     );
